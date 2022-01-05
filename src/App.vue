826576e7c607b7693784c616724509cc5e4d0f55
Guilherme da Costa <guilhermedc93@gmail.com>
2022-01-05 11:48:30 -0300

    Header and BaseCard components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
+<the-header title="RememberMe"/>
 <stored-resources :resources="storedResources" />
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/layouts/TheHeader.vue'
 export default {
-  components: { StoredResources },
+  components: { StoredResources, TheHeader },
   data() {
     return {
       storedResources: [
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue';
 import App from './App';
+import BaseCard from './components/UI/BaseCard';
 
 const app = createApp(App);
+app.component('base-card', BaseCard)
 app.mount('#app')
