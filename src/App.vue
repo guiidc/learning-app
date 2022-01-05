f7b22776389bcad3ebbff2ad4d9974f3a1c5fa8d
Guilherme da Costa <guilhermedc93@gmail.com>
2022-01-05 10:04:25 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The VueJS documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Search with google',
+          link: 'https://google.com',
+        },
+      ]
+    }
+  }
+}
+</script>
